82d972bb7cda890d9cd2688bd8a49b3b03cb8235
nathanael joseph <nathanaeljy@outlook.com>
2023-01-25 21:32:20 +0200

    created point json converter

--- Mapsharp.NetTopologySuite.GeoJson.Newtonsoft/Converters/ITypeConverter.cs
+++ Mapsharp.NetTopologySuite.GeoJson.Newtonsoft/Converters/ITypeConverter.cs
@@ -1,9 +1,7 @@
-﻿
-
-namespace Mapsharp.NetTopologySuite.GeoJson.Newtonsoft.Converters
+﻿namespace Mapsharp.NetTopologySuite.GeoJson.Newtonsoft.Converters
 {
     public interface ITypeConverter<in TFrom, out TTo>
     {
         TTo? Convert(TFrom? from);
-    }  
+    }
 }
---
+++ Mapsharp.NetTopologySuite.GeoJson.Newtonsoft/Extensions/SerializerSettingExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Mapsharp.NetTopologySuite.GeoJson.Newtonsoft.Converters;
+using NetTopologySuite.Geometries;
+using Newtonsoft.Json;
+
+namespace Mapsharp.NetTopologySuite.GeoJson.Newtonsoft.Extensions
+{
+    public static class SerializerSettingExtensions
+    {
+        public static JsonSerializerSettings AddNetTopologySuiteConverters(this JsonSerializerSettings settings, GeometryFactory factory)
+        {
+            settings.Converters.Add(CreatePointConverter(factory));
+
+            return settings;
+        }
+
+        private static TypeMappingJsonConverter<Point, Mapsharp.GeoJson.Core.Geometries.Point> CreatePointConverter(GeometryFactory factory)
+        {
+            var pointConverter = new PointConverter(factory);
+            return new TypeMappingJsonConverter<Point, Mapsharp.GeoJson.Core.Geometries.Point>(pointConverter, pointConverter);
+        }
+    }
+}
